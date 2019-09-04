380c833ba8de05e0ab3dc16ac2ff5314bc631c56
sakbayeme2015 <sakbayeme2015@gmail.com>
2019-09-04 17:02:13 -0400

    Add files via upload

---
+++ gistfile1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# JAVA_HOME=/your/java/home
+# R CMD javareconf -e
+# R
+# install.packages("rJava")
+# install.packages("RUnit")
+
+wget https://github.com/downloads/tc/RMongo/RMongo_0.0.20.tar.gz --no-check-certificate
+
+# R CMD install RMongo_0.0.20.tar.gz
+
+# R
+# install.packages("RMongo_0.0.20.tar.gz")
+# library("RMongo")
